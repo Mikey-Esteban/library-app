46e15265738f11d3953ef10118724dcb7f80b266
Mikey Esteban <michaelp.esteban@gmail.com>
2020-11-17 14:36:12 -0500

    deleted comments

--- src/myLibrary.js
+++ src/myLibrary.js
@@ -1,35 +1,22 @@
 const myLibrary = (() => {
 
   let library;
-  // let count;
 
   // Lookup localStorage for library
   const result = myStorage.getItem('library');
   if (result) {
     library = JSON.parse(result);
-    // count = library.length
-
-    // myStorage.setItem('count', library.length)
-    console.log(`We have local storage!: ${library}`);
-    // console.log(`Count is now: ${count}`);
-
-
-    // UI function... why is it here?
-    // for (let book of library) {
-    //   updateBooksList(book);
-    // }
   } else {
     library = [];
-    // count = 0;
+    // initialize count for book ids
     myStorage.setItem('count', '0');
   }
 
   const addBookToLibrary = (book) => {
     library.push(book);
-    // count++;
 
     //local storage
-    myStorage.setItem('library', JSON.stringify(library))
+    myStorage.setItem('library', JSON.stringify(library));
     const newCount = parseInt(myStorage.getItem('count')) + 1;
     myStorage.setItem('count', newCount);
   }
@@ -38,7 +25,7 @@ const myLibrary = (() => {
     library = library.filter( book => book.id !== id);
 
     //local storage
-    myStorage.setItem('library', JSON.stringify(library))
+    myStorage.setItem('library', JSON.stringify(library));
   }
 
   const editBookReadStatus = (book) => {
@@ -48,15 +35,14 @@ const myLibrary = (() => {
   const clearStorage = () => {
     // Remove each Book from Menu
     for (let book of library) {
-      removeBookFromLibrary(book, book.id)
+      removeBookFromLibrary(book, book.id);
     }
     // clear myStorage cache
-    myStorage.clear()
+    myStorage.clear();
   }
 
   return {
     library,
-    // count,
     addBookToLibrary,
     editBookReadStatus,
     removeBookFromLibrary,
